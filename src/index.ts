cc8cc858cdc9f76248bb2848ead4d71cef0aa6ce
bbaldino <bbaldino@cisco.com>
2021-08-18 13:00:58 -0700

    fix: fix the way we do exports (#19)
    * fix: fix the way we do exports
    
    * fix: fix path in api-extractor file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-export * as media from './media';
-export * as peerConnection from './peer-connection';
+// Note: This (importing and then exporting media) is a workaround for doc extraction, which doesn't
+// support 'export * as ___ from ___' yet.
+import * as media from './media';
+
+export { media };
+
+export * from './peer-connection';
