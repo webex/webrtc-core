9a28b2d8551e7d2dd2444a778acaec4090010504
arungane <arungane@cisco.com>
2022-10-17 22:44:24 -0400

    feat(device-manager) : options to create custom track and buffered audio track

--- src/device/device-management.ts
+++ src/device/device-management.ts
@@ -12,7 +12,7 @@ export enum ErrorTypes {
 /**
  * Represents a WCME error, which contains error type and error message.
  */
-export class WcmeError {
+export class WebrtcError {
   type: string;
 
   message: string;
@@ -33,14 +33,73 @@ export type AudioDeviceConstraints = {
   deviceId?: string;
 };
 
-export type VideoDeviceConstraints = {
-  deviceId?: ConstrainDOMString;
-  width?: ConstrainULong;
-  height?: ConstrainULong;
-  aspectRatio?: ConstrainDouble;
-  frameRate?: ConstrainDouble;
-  facingMode?: ConstrainDOMString;
+// Predefined encoder config mentioned similar to https://api-ref.agora.io/en/voice-sdk/web/4.x/globals.html#videoencoderconfigurationpreset
+export enum StaticVideoEncoderConfig {
+  Profile1080P = '1080P',
+}
+
+export type ConstrainLong = {
+  min?: number;
+  max?: number;
+  ideal?: number;
+  exact?: number;
 };
+
+// Predefined encoder config mentioned similar to https://api-ref.agora.io/en/voice-sdk/web/4.x/globals.html#videoencoderconfigurationpreset
+export type videoEncoderConfig = {
+  bitRateMin?: number;
+  bitrateMax?: number;
+  frameRate?: number | ConstrainLong;
+  height?: number | ConstrainLong;
+  width?: number | ConstrainLong;
+  aspectRatio?: number;
+};
+
+export type VideoConstraints = {
+  cameraDeviceId?: string;
+  faceingMode?: ConstrainULong;
+  optimizationMode?: ConstrainULong;
+  encoderConfig?: StaticVideoEncoderConfig | videoEncoderConfig; 
+};
+
+// something like this https://api-ref.agora.io/en/video-sdk/web/4.x/globals.html#audioencoderconfigurationpreset
+export enum StaticAudioEncoderConfig {
+  sampleRateLow = '16kHz',
+}
+
+export type audioEncoderConfig = {
+  bitrate?: number;
+  sampleRate?: number;
+  sampleSize?: number;
+  stereo?: boolean;
+};
+
+export type MicrophoneConstraints = {
+  echoCancelation?: boolean;
+  autoGainControl?: boolean;
+  noiseSupression?: boolean;
+  microphoneDeviceid?: string;
+  encoderConfig?: StaticAudioEncoderConfig | audioEncoderConfig;
+};
+
+export type CustomVideoTrackInitConfig = {
+  mediaStreamTrack: MediaStreamTrack;
+  encoderConfig?: StaticVideoEncoderConfig | videoEncoderConfig;
+};
+
+export type CustomAudioTrackInitConfig = {
+  mediaStreamTrack: MediaStreamTrack;
+  encoderConfig?: StaticAudioEncoderConfig | audioEncoderConfig;
+};
+
+/**
+ * @param audioConfig
+ * @param videoConfig
+ */
+export async function createMicrophoneAndCameraTracks(
+  audioConfig?: MicrophoneConstraints,
+  videoConfig?: VideoConstraints
+): Promise<[IMicrophoneAudioTrack, ICameraVideoTrack]> {}
 
 /**
  * Creates a camera video track.
@@ -48,14 +107,13 @@ export type VideoDeviceConstraints = {
  * @param constraints - Video device constraints.
  * @returns A LocalTrack object or an error.
  */
-export async function createCameraTrack(
-  constraints?: VideoDeviceConstraints
-): Promise<LocalCameraTrack> {
+export async function createCameraTrack(constraints?: VideoConstraints): Promise<LocalCameraTrack> {
   let stream: MediaStream;
   try {
-    stream = await media.getUserMedia({ video: { ...constraints } });
+    // TODO: change it
+    stream = await media.getUserMedia({ video: {} });
   } catch (error) {
-    throw new WcmeError(
+    throw new WebrtcError(
       ErrorTypes.CREATE_CAMERA_TRACK_FAILED,
       `Failed to create camera track ${error}`
     );
@@ -76,22 +134,52 @@ export async function createMicrophoneTrack(
   try {
     stream = await media.getUserMedia({ audio: { ...constraints } });
   } catch (error) {
-    throw new WcmeError(
+    throw new WebrtcError(
       ErrorTypes.CREATE_MICROPHONE_TRACK_FAILED,
       `Failed to create microphone track ${error}`
     );
   }
+  // See if we can just pass the track and not streams
   return new LocalMicrophoneTrack(stream);
 }
 
 /**
  * Creates a display video track.
  *
+ * @param constraints
  * @returns A Promise that resolves to a LocalDisplayTrack.
  */
-export async function createDisplayTrack(): Promise<LocalDisplayTrack> {
+export async function createDisplayTrack(
+  constraints?: VideoDeviceConstraints
+): Promise<LocalDisplayTrack> {
   const stream = await media.getDisplayMedia({ video: true });
   return new LocalDisplayTrack(stream);
+}
+
+/**
+ * Creates custom video track .
+ *
+ * @param constraints
+ * @returns A Promise that resolves to a LocalVideoTrack.
+ */
+export async function createCustomVideoTrack(
+  constraints?: CustomVideoTrackInitConfig
+): Promise<LocalVideoTrack> {
+  const stream = await media.getUserMedia({ video: constraints });
+  return new LocalVideoTrack(stream);
+}
+
+/**
+ * Creates custom audio track .
+ *
+ * @param constraints
+ * @returns A Promise that resolves to a LocalAudioTrack.
+ */
+export async function createCustomAudioTrack(
+  constraints?: CustomVideoTrackInitConfig
+): Promise<LocalAudioTrack> {
+  const stream = await media.getUserMedia({ audio: constraints });
+  return new LocalAudioTrack(stream);
 }
 
 /**
@@ -108,7 +196,10 @@ export async function getDevices(deviceKind?: media.DeviceKind): Promise<MediaDe
       media.enumerateDevices
     );
   } catch (error) {
-    throw new WcmeError(ErrorTypes.DEVICE_PERMISSION_DENIED, 'Failed to ensure device permissions');
+    throw new WebrtcError(
+      ErrorTypes.DEVICE_PERMISSION_DENIED,
+      'Failed to ensure device permissions'
+    );
   }
 
   return devices.filter((v: MediaDeviceInfo) => (deviceKind ? v.kind === deviceKind : true));
