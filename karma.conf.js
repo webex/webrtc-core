a9fdd3c7ed0fa8552ba1b24660d6d8530db92948
Filip Nowakowski <fnowakow@cisco.com>
2026-07-26 23:03:46 +0200

    chore: update esbuild-plugin-istanbul filter for TypeScript files

--- karma.conf.js
+++ karma.conf.js
@@ -305,7 +305,7 @@ module.exports = (config) => {
       target: 'es2015',
       plugins: [
         esbuildPluginIstanbul({
-          filter: /\.ts$/,
+          filter: /(?:^|[/\\])[^/\\.]+\.ts$/,
           loader: 'ts',
           name: 'istanbul-loader-ts',
         }),
